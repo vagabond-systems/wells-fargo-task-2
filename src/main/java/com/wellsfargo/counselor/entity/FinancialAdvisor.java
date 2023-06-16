2f34305409e4a625f50b0f80a1df416cdb39ef7e
Jyotiraditya Sikder <92026938+astroraditya@users.noreply.github.com>
2023-06-16 11:05:13 +0530

    Update FinancialAdvisor.java

--- src/main/java/com/wellsfargo/counselor/entity/FinancialAdvisor.java
+++ src/main/java/com/wellsfargo/counselor/entity/FinancialAdvisor.java
@@ -12,34 +12,25 @@ public class FinancialAdvisor {
     private Long id;
     private String name;
     private String contactDetails;
-
     public FinancialAdvisor() {
     }
-
     public FinancialAdvisor(String name, String contactDetails) {
         this.name = name;
         this.contactDetails = contactDetails;
     }
-
-    // Getters and Setters
     public Long getId() {
         return id;
     }
-
     public String getName() {
         return name;
     }
-
     public void setName(String name) {
         this.name = name;
     }
-
     public String getContactDetails() {
         return contactDetails;
     }
-
     public void setContactDetails(String contactDetails) {
         this.contactDetails = contactDetails;
     }
 }
-
